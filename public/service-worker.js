67d778a837bfb7f89f6905c90e2337fdbbd82af0
Luiz Eduardo Nogueira <luizetn@gmail.com>
2020-10-27 09:20:36 -0300

    versao 27/10/2020 09:20

--- public/service-worker.js
+++ public/service-worker.js
@@ -1,4 +1,4 @@
-importScripts("/precache-manifest.4573a4eae8c2d5617978889ea73f000e.js", "https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
+importScripts("/precache-manifest.078b22e3467ecb63b06fce3bea76c8f2.js", "https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 /*
  * This file (which will be your service worker)
