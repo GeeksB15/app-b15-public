9059889dbac17f75611d384d277497349975dd0d
Luiz Eduardo Nogueira <luizetn@gmail.com>
2021-01-26 17:32:58 -0300

    versao 26/01/2021 17:27

--- public/service-worker.js
+++ public/service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.32a2b5670b72f9eec12efe1994ffd5e4.js"
+  "/precache-manifest.482fbd3ce0224af35d7137b684febbf2.js"
 );
 
 workbox.core.setCacheNameDetails({prefix: "b15"});
