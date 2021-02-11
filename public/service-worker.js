487e6dc61d200510f00d31d9fab759ba09530704
Carlos Neves Lenz Cesar <cnlenzc@gmail.com>
2021-02-11 09:56:19 -0300

    2021-02-11 9h56

--- public/service-worker.js
+++ public/service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.ca861aa3d266cf2ccbb86115de84ca31.js"
+  "/precache-manifest.e8263896e5e951a46e32be78859eade2.js"
 );
 
 workbox.core.setCacheNameDetails({prefix: "b15"});
