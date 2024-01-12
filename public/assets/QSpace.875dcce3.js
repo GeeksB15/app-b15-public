bcee11775c28d87e9d29c75e9f4393c469ffedf3
luiznogueira <luizetn@gmail.com>
2024-01-12 10:40:02 -0300

    versao 12/01/2024 10:40

---
+++ public/assets/QSpace.875dcce3.js
@@ -0,0 +1,1 @@
+import{a7 as a,an as e}from"./index.2840a24b.js";const s=a("div",{class:"q-space"});var c=e({name:"QSpace",setup(){return()=>s}});export{c as Q};
---
+++ public/assets/axios.37826b88.js
@@ -0,0 +1,1 @@
+import{b as s,a as o}from"./index.2840a24b.js";import{a as e}from"./index.2840a24b.js";var r=s(({app:a})=>{a.config.globalProperties.$axios=o,globalThis.$axios=o});export{e as axios,r as default};
