a5e3ea875751e054376dd6a79a88b4c90e3997fd
luiznogueira <luizetn@gmail.com>
2024-01-18 14:53:19 -0300

    versao 18/01/2024 14:52

---
+++ public/assets/imprimir.d1da336a.js
@@ -0,0 +1,1 @@
+import{b as n,a1 as s,a2 as b}from"./index.de1453f2.js";var f=n(({app:r,router:e})=>{const o=function(l,i,m){i=i?typeof i=="object"?s.encodeToUrl(JSON.stringify(i)):s.encodeToUrl(i):"",b(m),e.push({name:"Imprimir",params:{nome:l,parametro:i}})};r.config.globalProperties.$imprimir=o,globalThis.$imprimir=o});export{f as default};
