ddcf6316099d7a5db747a542d79aa7a68b499aea
luiznogueira <luizetn@gmail.com>
2024-01-23 17:14:51 -0300

    versao 23/01/2024 17:12

---
+++ public/assets/axios.d76099c0.js
@@ -0,0 +1,1 @@
+import{b as s,a as o}from"./index.ee33a5e1.js";import{a as e}from"./index.ee33a5e1.js";var r=s(({app:a})=>{a.config.globalProperties.$axios=o,globalThis.$axios=o});export{e as axios,r as default};
