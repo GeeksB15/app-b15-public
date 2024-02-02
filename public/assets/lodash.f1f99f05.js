fd603699f4e05658ab132343434388abf35c516d
Carlos Neves Lenz Cesar <cnlenzc@gmail.com>
2024-02-02 10:19:47 -0300

    versao: v2024.0202.1006

---
+++ public/assets/lodash.f1f99f05.js
@@ -0,0 +1,1 @@
+import{b as l,a3 as o}from"./index.68cc8507.js";var e=l(({app:a})=>{a.config.globalProperties.$lodash=o,globalThis.$lodash=o,a.config.globalProperties.$get=o.get,globalThis.$get=o.get});export{e as default};
--- public/assets/utils.e000226a.js
+++
@@ -1,1 +0,0 @@
-import{b as s,ag as l}from"./index.a5b30aa8.js";var a=s(({app:o})=>{o.config.globalProperties.$utils=l,globalThis.$utils=l});export{a as default};
