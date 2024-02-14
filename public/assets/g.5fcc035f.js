7b572659f5fe926bde7acda7a4072321bbcb8847
Carlos Neves Lenz Cesar <cnlenzc@gmail.com>
2024-02-14 14:43:34 -0300

    versao: v2024.0214.1439

---
+++ public/assets/g.5fcc035f.js
@@ -0,0 +1,1 @@
+import{b as a}from"./index.f980bb69.js";const o={};var t=a(({app:g})=>{g.config.globalProperties.$g=o,globalThis.$g=o});export{t as default};
---
+++ public/assets/utils.360f4d83.js
@@ -0,0 +1,1 @@
+import{b as s,ag as l}from"./index.f980bb69.js";var a=s(({app:o})=>{o.config.globalProperties.$utils=l,globalThis.$utils=l});export{a as default};
