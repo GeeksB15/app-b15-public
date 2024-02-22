13a23bc22066daa372cdb071d1f449962edbe29e
luiznogueira <luizetn@gmail.com>
2024-02-22 12:30:32 -0300

    versao 22/02/2024 12:27

---
+++ public/assets/lodash.c105401c.js
@@ -0,0 +1,1 @@
+import{b as l,a3 as o}from"./index.7a11122e.js";var e=l(({app:a})=>{a.config.globalProperties.$lodash=o,globalThis.$lodash=o,a.config.globalProperties.$get=o.get,globalThis.$get=o.get});export{e as default};
