5ec4eee8615638e488b07dc0a664842ab7f96f6a
Carlos Neves Lenz Cesar <cnlenzc@gmail.com>
2024-02-29 10:38:47 -0300

    versao: v2024.0229.1029

---
+++ public/assets/lodash.bdc4c067.js
@@ -0,0 +1,1 @@
+import{b as l,a3 as o}from"./index.d6ce7622.js";var e=l(({app:a})=>{a.config.globalProperties.$lodash=o,globalThis.$lodash=o,a.config.globalProperties.$get=o.get,globalThis.$get=o.get});export{e as default};
