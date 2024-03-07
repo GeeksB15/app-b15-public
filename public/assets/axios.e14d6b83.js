f51bbe78af444eee9e32960faaa36dd7e823d1d1
Carlos Neves Lenz Cesar <cnlenzc@gmail.com>
2024-03-07 11:10:44 -0300

    versao: v2024.0307.1105

---
+++ public/assets/axios.e14d6b83.js
@@ -0,0 +1,1 @@
+import{b as s,a as o}from"./index.8af65ecd.js";import{a as e}from"./index.8af65ecd.js";var r=s(({app:a})=>{a.config.globalProperties.$axios=o,globalThis.$axios=o});export{e as axios,r as default};
---
+++ public/assets/utils.8f0a33e6.js
@@ -0,0 +1,1 @@
+import{b as s,ag as l}from"./index.8af65ecd.js";var a=s(({app:o})=>{o.config.globalProperties.$utils=l,globalThis.$utils=l});export{a as default};
